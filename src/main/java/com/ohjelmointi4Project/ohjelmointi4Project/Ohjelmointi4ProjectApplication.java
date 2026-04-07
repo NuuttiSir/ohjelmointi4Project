47d4514cc11ce49e49e55d0bb8aa7ccda8e1676e
nuuttisir <nuutti.sirvio@gmail.com>
2026-04-07 16:23:27 +0300

    added index html file, added bootstrap icons to index file

--- src/main/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplication.java
+++ src/main/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplication.java
@@ -2,12 +2,18 @@ package com.ohjelmointi4Project.ohjelmointi4Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Ohjelmointi4ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ohjelmointi4ProjectApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String frontPage() {
+		return "index.html";
 	}
 
 }
