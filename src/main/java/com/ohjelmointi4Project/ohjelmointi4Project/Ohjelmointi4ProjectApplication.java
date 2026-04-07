fcb83422fd95816ac9e66b24c95687ec870e66a7
nuuttisir <nuutti.sirvio@gmail.com>
2026-04-07 13:50:44 +0300

    initiated spring boot things

---
+++ src/main/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohjelmointi4Project.ohjelmointi4Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ohjelmointi4ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ohjelmointi4ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohjelmointi4Project.ohjelmointi4Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ohjelmointi4ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
