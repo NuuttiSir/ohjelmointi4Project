89316a600981873f8d15cf67a3edde74a9424ca8
nuuttisir <nuutti.sirvio@gmail.com>
2026-04-08 11:33:49 +0300

    started on the frontpage

--- src/main/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplication.java
+++ src/main/java/com/ohjelmointi4Project/ohjelmointi4Project/Ohjelmointi4ProjectApplication.java
@@ -1,11 +1,20 @@
 package com.ohjelmointi4Project.ohjelmointi4Project;
 
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Ohjelmointi4ProjectApplication {
+
+	private final DatabaseConf db = new DatabaseConf();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ohjelmointi4ProjectApplication.class, args);
@@ -16,4 +25,14 @@ public class Ohjelmointi4ProjectApplication {
 		return "index.html";
 	}
 
+	@PostMapping("/preview")
+	@ResponseBody
+	public String inputtedTextToShow(@RequestParam(value = "demo-multi-string", defaultValue = "") String text) {
+		try {
+			db.insertMessage(text);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "<p>" + text + "</p>";
+	}
 }
